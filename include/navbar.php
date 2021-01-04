58ebcb42998e3a1ab82219609254e964a255858b
LeDu98 <dulegajic98@gmail.com>
2021-01-04 22:45:33 +0100

    dodat bootstrap i ulepsano

---
+++ include/navbar.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#">Autoprevoznicka radnja</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li class="active"><a href="index.php">Pocetna stranica</a></li>
+        <li><a href="dodaj.php">Dodaj</a></li>
+        <li><a href="#">Projects</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+      
+    </div>
+  </div>
+</nav>
--- index.php
+++ index.php
@@ -1,16 +1,68 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<script type="text/javascript" src="obrisi.js"></script>
+  <title>Autoprevoznicka radnja</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1" http-equiv="Content-Type">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="obrisi.js"></script>
+  <style>
+    /* Remove the navbar's default margin-bottom and rounded borders */ 
+    .navbar {
+      margin-bottom: 0;
+      border-radius: 0;
+    }
+    
+    /* Set height of the grid so .sidenav can be 100% (adjust as needed) */
+    .row.content {height: 450px}
+    
+    /* Set gray background color and 100% height */
+    .sidenav {
+      padding-top: 20px;
+      background-color: #f1f1f1;
+      height: 100%;
+    }
+    
+    /* Set black background color, white text and some padding */
+    footer {
+      background-color: #555;
+      color: white;
+      padding: 15px;
+    }
+    
+    /* On small screens, set height to 'auto' for sidenav and grid */
+    @media screen and (max-width: 767px) {
+      .sidenav {
+        height: auto;
+        padding: 15px;
+      }
+      .row.content {height:auto;} 
+    }
+  </style>
 </head>
 <body>
-<div name="tabela_tura">
-<?php
+<?php require "include/navbar.php"?>
+
+  
+<div class="container-fluid text-center">    
+  <div class="row content">
+    <div class="col-sm-2 sidenav">
+      
+    </div>
+    <div class="col-sm-8 text-left"> 
+      <h1>Dobrodosli na sajt autoprevoznicke radnje</h1>
+      <p>Sajt je osmisljen kako bi olaksao vlasnicima evidenciju o turama i olaksao manipulisanje podacima o njima. Takodje moguce je voditi i evidenciju o vozacima i kamionima koji ucestvuju u istim.</p>
+      <hr>
+      <div id="tabela_tura" name="tabela_tura">
+      <h3>Prikaz svih tura</h3>
+      <?php
 include "konekcija.php";
 
 $sql="SELECT idtura,ruta,napomena, datum, CONCAT(ime,' ',prezime) as imeprezime, reg_br FROM tura,vozac,kamion
-where tura.idvozac=vozac.idvozac and tura.idkamion=kamion.idkamion";
+where tura.idvozac=vozac.idvozac and tura.idkamion=kamion.idkamion
+order by datum desc";
 if (!$q=$mysqli->query($sql)){
 die ("Nastala je greška pri izvođenju upita<br/>" . $mysqli->error);
 }
@@ -20,7 +72,7 @@ echo "Nema tura";
 else {
 //prelazi se u HTML ispis
 ?>
-<table id="ta" width="600" border="1" cellpadding="5" cellspacing="2" style="text-align:center ">
+<table id="ta" width="1000" height="200" border="4" cellpadding="10" cellspacing="5" style="text-align:center ">
 <tr>
 <td><b>Ruta</b></td>
 <td><b>Napomena</b></td>
@@ -45,7 +97,21 @@ while ($red=$q->fetch_object()){
 }
 $mysqli->close();
 ?>
+    
+  </div>
 </div>
+</div>
+
+<footer class="container-fluid text-center">
+  <p>Autoprevoznicka radnja</p>
+  <p>Telefon: 011/8000-800</p>
+  <p>e-mail: apr@support.com</p>
+  
+</footer>
 
 </body>
 </html>
+
+
+
+
